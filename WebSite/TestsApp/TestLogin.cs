ff8e9946fbf1f9fb6d695c20b99ec9a666cacc86
Jason David Espinoza Siles <espinoza.jasondavid@gmail.com>
2017-11-08 16:32:01 -0600

    Add files via upload

--- WebSite/TestsApp/TestLogin.cs
+++ WebSite/TestsApp/TestLogin.cs
@@ -25,60 +25,61 @@ namespace LoginModelApp
         public Test() {
         }
 
+
+        //UC-001: Acceder al sistema.
+        //      TC-01: Acceder al sistema como administrador.
         [TestCase ("admin@gmail.com","1234")]
-        //[UnidadDeTrabajo][Contexto][Resultado]
+       
         public void LoginUser_ValidData_StartUserSession(String userName, String password)
         {
 
             int state;
-            //enviar usuario y contraseña.
-            //Login.verificarInicioSesion("","");
             LoginModel vlogin = new LoginModel(userName, password);
             state = vlogin.verificarInicioSesion();
             Assert.AreEqual(1,state);
             
         }
 
-        //Usuario inválida
+
+        //UC-001: Acceder al sistema.
+        //      TC-01: Acceder al sistema como inválido.
+        
         [TestCase("adminzzz@gmail.com", "1234")]
         public void LoginUser_InvalidDataUserName_StartUserSession(String userName, String password)
         {
             int state;
-            //enviar usuario y contraseña.
-            //Login.verificarInicioSesion("","");
             LoginModel vlogin = new LoginModel(userName, password);
             state = vlogin.verificarInicioSesion();
             Assert.AreEqual(-1, state);
 
         }
 
-        //Contraseña inválida
+        //UC-001: Acceder al sistema.
+        //      TC-03: Acceder al sistema con contraseña inválida.
         [TestCase("admin@gmail.com", "12434")]
         public void LoginUser_InvalidDataPassword_StartUserSession(String userName, String password)
         {
             int state;
-            //enviar usuario y contraseña.
-            //Login.verificarInicioSesion("","");
             LoginModel vlogin = new LoginModel(userName, password);
             state = vlogin.verificarInicioSesion();
             Assert.AreEqual(-1, state);
 
         }
 
-        //No ingresa contraseña
+        //UC-001: Acceder al sistema.
+        //      TC-04: Acceder al sistema No se ingresa la contaseña.
         [TestCase("admin@gmail.com", "")]
         public void LoginUser_InvalidDataNoPassword_StartUserSession(String userName, String password)
         {
             int state;
-            //enviar usuario y contraseña.
-            //Login.verificarInicioSesion("","");
             LoginModel vlogin = new LoginModel(userName, password);
             state = vlogin.verificarInicioSesion();
             Assert.AreEqual(-1, state);
 
         }
 
-        //No ingresa usuario
+        //UC-001: Acceder al sistema.
+        //      TC-05: Acceder al sistema sin usuario.
         [TestCase("", "1234")]
         public void LoginUser_InvalidDataNoUserName_StartUserSession(String userName, String password)
         {
@@ -91,31 +92,29 @@ namespace LoginModelApp
 
         }
 
-        //UC-003:
-        //        TC-009
-        //[UnidadDeTrabajo][Contexto][Resultado]
-        [TestCase("josecarlos@gmail.com", "1234")]
+        //UC-001: Acceder a listado de Empresas.
+        //      TC-076: Acceder al sistema como usuario.
+
+        [TestCase("bernan0795@gmail.com", "1234")]
         public void SalesInfo_AvialableCorporationInformation_ShowCorporationInformation(String userName, String password)
         {
             int state;
-            //enviar usuario y contraseña.
-            //Login.verificarInicioSesion("","");
+            
             LoginModel vlogin = new LoginModel(userName, password);
             state = vlogin.verificarInicioSesion();
-            Assert.AreEqual(3, state);
+            Assert.AreEqual(0, state);
 
         }
 
         //UC-004: Acceder a listado de Empresas.
         //      TC-012: Acceder a la lista de contactos como usuario.
-        //[UnidadDeTrabajo][Contexto][Resultado]
+        
 
         [TestCase("josecarlos@gmail.com", "1234")]
         public void ContactInfo_AvialableCorporationInformation_ShowCoporationInformation(String userName, String password)
         {
             int state;
-            //enviar usuario y contraseña.
-            //Login.verificarInicioSesion("","");
+            
             LoginModel vlogin = new LoginModel(userName, password);
             state = vlogin.verificarInicioSesion();
             Assert.AreEqual(3, state);
@@ -124,10 +123,10 @@ namespace LoginModelApp
 
         //UC-004: Acceder a listado de Empresas.
         //      TC-015: Lista de contactos está vacía
-        //[UnidadDeTrabajo][Contexto][Resultado]
+        
 
         [TestCase]
-        public void CorpInfo_AccessCorpInformatin_DontExistsContacts()
+        public void CorpInfo_AccessCorpInformatin_DontExistsCorporations()
         {
 
             
@@ -141,9 +140,9 @@ namespace LoginModelApp
 
         //UC-004: Acceder a listado de contactos registrados.
         //      TC-014: Lista de contactos no está vacía
-        //[UnidadDeTrabajo][Contexto][Resultado]
+        
 
-        //[TestCase]
+        [TestCase]
         public void CorptInfo_AccessCorpInformatin_ExistsCorp()
         {
 
